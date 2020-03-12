89166658f3474ce67c93cd05854df2d10bd574bb
TREY <hugh.a.lloyd@gmail.com>
2020-03-11 22:45:12 -0500

    finishing custom middleware, need to add to endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const UserDb = require('./userDb');
+const PostsDb = require('../posts/postDb');
+
+
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  UserDb.insert(req.body)
+  .then (response=>{
+    res.status(201).json(response)
+    console.log(response,'res from post to /')
+  })
+  .catch(err=>({message:'server error'}))
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -38,6 +48,7 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
+  !req.body?res.status(400).json({message:'missing user data'}):!req.body.name?res.status(400).json({message:'missing name field'}):next()
 }
 
 function validatePost(req, res, next) {
